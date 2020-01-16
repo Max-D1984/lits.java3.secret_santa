3e1a843f58fb4cf90437dfb230418468a423d834
olehkhalus <oleh0704@gmail.com>
2020-01-16 19:42:36 +0200

    update Service package data

---
+++ src/main/java/service/RuleService.java
@@ -0,0 +1,15 @@
+package service;
+
+
+public interface RuleService {
+    void updateRule();
+
+    void deleteRule();
+
+    void createRule();
+
+   void readRule();
+
+   void readRuleList();
+
+}

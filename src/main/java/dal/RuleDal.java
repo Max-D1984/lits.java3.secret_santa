4d03acb6be3854cd1b30b0ca0fe9ccdf3df21f65
olehkhalus <oleh0704@gmail.com>
2020-01-16 19:47:16 +0200

    add RuleDal and RulDalImpl

---
+++ src/main/java/dal/RuleDal.java
@@ -0,0 +1,9 @@
+package dal;
+
+public interface RuleDal {
+    public void create ();
+    public void read ();
+    public void update ();
+    public void delete ();
+    public void readList ();
+}
---
+++ src/main/java/dal/RuleDalImpl.java
@@ -0,0 +1,29 @@
+package dal;
+
+public class RuleDalImpl implements RuleDal {
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void readList() {
+
+    }
+}

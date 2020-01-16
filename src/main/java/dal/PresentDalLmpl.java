65c66115135aabe5d85ad71027a57843de0668a6
Panda1958 <andy.panda1958@gmail.com>
2020-01-16 19:41:22 +0300

    change PresentDalLmpl

---
+++ src/main/java/dal/PresentDalLmpl.java
@@ -0,0 +1,30 @@
+package dal;
+
+public class PresentDalLmpl implements PresentDal {
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void readList() {
+
+    }
+}

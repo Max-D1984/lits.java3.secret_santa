428f5a8156aea3efe4f42e381dd07ee906b82e0f
Sedel Zoryana <zoryanasedel@gmail.com>
2020-01-16 19:37:35 +0200

    Changes creation Pojo class

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,65 @@
+package pojo;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String userName;
+    private String userRole;
+
+
+    public User(int id, String userName, String userRole) {
+        this.id = id;
+        this.userName = userName;
+        this.userRole = userRole;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User that = (User) o;
+        return id == that.id &&
+                Objects.equals(userName, that.userName) &&
+                Objects.equals(userRole, that.userRole);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, userRole);
+    }
+
+    @Override
+    public String toString() {
+        return "UserRecords{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", userRole='" + userRole + '\'' +
+                '}';
+    }
+}
+

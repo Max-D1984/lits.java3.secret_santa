2a780624ba77847933918c7e91737a7045e8e03a
Panda1958 <andy.panda1958@gmail.com>
2020-01-16 19:34:57 +0300

    change PresentDal

--- src/main/java/dal/PresentDal.java
+++ src/main/java/dal/PresentDal.java
@@ -1,4 +1,12 @@
 package dal;
 
 public interface PresentDal {
+    public void create ();
+    public void read ();
+    public void update ();
+    public void delete ();
+    public void readList ();
+
+
+
 }

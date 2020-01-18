b2c1be83e58cb25de7b00123fde5c2c709f755d2
Max-D1984 <is96071@gmail.com>
2020-01-18 12:42:10 +0200

    Add CompanyDal, CompanyDalImp, SQL Company

---
+++ src/main/java/dal/CompanyDal.java
@@ -0,0 +1,13 @@
+package dal;
+
+import pojo.Company;
+
+import java.util.List;
+
+public interface CompanyDal {
+    public void create (Company present);
+    public Company read (long id);
+    public void update (long id, Company company);
+    public void delete (long id);
+    public List<Company> readList ();
+}
---
+++ src/main/java/dal/CompanyDalImpl.java
@@ -0,0 +1,32 @@
+package dal;
+
+import pojo.Company;
+
+import java.util.List;
+
+public class CompanyDalImpl implements CompanyDal{
+    @Override
+    public void create(Company present) {
+
+    }
+
+    @Override
+    public Company read(long id) {
+        return null;
+    }
+
+    @Override
+    public void update(long id, Company pres) {
+
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public List<Company> readList() {
+        return null;
+    }
+}

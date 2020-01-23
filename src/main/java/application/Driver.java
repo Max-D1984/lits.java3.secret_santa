fae384c3497525faba83da9556ee6b141c601e1c
Oleh Khalus <oleh0704@gmail.com>
2020-01-23 17:27:43 +0200

    add new class Driver (only connection to DB) and file database.properties

---
+++ src/main/java/application/Driver.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            try (Connection conn = getConnection()){
+
+                System.out.println("Connection to Store DB succesfull!");
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException, IOException {
+
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get("database.properties"))){
+            props.load(in);
+        }
+        String url = props.getProperty("url");
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
+
+        return DriverManager.getConnection(url, username, password);
+    }
+}
+

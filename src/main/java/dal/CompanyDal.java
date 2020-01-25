94e3f448f298d461f4795443ce6510e64eaae6a0
Max-D1984 <is96071@gmail.com>
2020-01-25 11:12:59 +0200

    Upd CompanyDal

--- src/main/java/dal/CompanyDal.java
+++ src/main/java/dal/CompanyDal.java
@@ -5,7 +5,7 @@ import pojo.Company;
 import java.util.List;
 
 public interface CompanyDal {
-    public void create (Company present);
+    public void create (Company company);
     public Company read (long id);
     public void update (long id, Company company);
     public void delete (long id);
--- src/main/java/dal/CompanyDalImpl.java
+++ src/main/java/dal/CompanyDalImpl.java
@@ -1,89 +1,96 @@
 package dal;
 
 import pojo.Company;
+import application.Driver;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.LinkedList;
 import java.util.List;
 
 public class CompanyDalImpl implements CompanyDal {
 
-
-    private Connection conn;
-    private Statement stm;
+    //
+//    private Connection conn;
+//    private Statement stm;
     private ResultSet resultSet;
-    private String urlToBase;
-    private String baseName;
-    private String user;
-    private String password;
-
-
-    public CompanyDalImpl(String urlToBase, String baseName, String user, String password) {
-        this.urlToBase = urlToBase;
-        this.baseName = baseName;
-        this.user = user;
-        this.password = password;
-        connectionToBase();
-    }
-
-
-    private boolean connectionToBase() {
-        try {
-            String connectionUrl = "jdbc:jtds:sqlserver://" + urlToBase + ";databaseName=" + baseName;
-            conn = DriverManager.getConnection(connectionUrl, user, password);
-            stm = conn.createStatement();
-            return true;
-        } catch (Exception ex) {
-            System.out.println(ex);
-            return false;
-        }
-
-    }
+//    private String urlToBase;
+//    private String baseName;
+//    private String user;
+//    private String password;
+//
+//
+//    public CompanyDalImpl(String urlToBase, String baseName, String user, String password) {
+//        this.urlToBase = urlToBase;
+//        this.baseName = baseName;
+//        this.user = user;
+//        this.password = password;
+//        connectionToBase();
+//    }
+//
+//
+//    private boolean connectionToBase() {
+//        try {
+//            String connectionUrl = "jdbc:jtds:sqlserver://" + urlToBase + ";databaseName=" + baseName;
+//            conn = DriverManager.getConnection(connectionUrl, user, password);
+//            stm = conn.createStatement();
+//            return true;
+//        } catch (Exception ex) {
+//            System.out.println(ex);
+//            return false;
+//        }
+//
+//    }
 
 
     @Override
     public void create(Company company) {
         String sql = "INSERT INTO [company1] (name, description) VALUES (?,?)";
         try {
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            PreparedStatement preparedStatement = Driver.getConnection().prepareStatement(sql);
             preparedStatement.setString(1, company.getCompanyName());
             preparedStatement.setString(2, company.getCompanyDescription());
             preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
+        } catch (SQLException | IOException ex) {
             System.out.println("Company creation failed");
         }
     }
 
     @Override
     public Company read(long id) {
-        Company company;
+        Company company = new Company(0,null,null);
         try {
-            String sql = "SELECT id, name, description FROM people WHERE id=?";
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            String sql = "SELECT id, name, description FROM [company1] WHERE id=?";
+            PreparedStatement preparedStatement = Driver.getConnection().prepareStatement(sql);
             preparedStatement.setLong(1, id);
-
             resultSet = preparedStatement.executeQuery();
-            int resultId = resultSet.getInt("id");
-            String resultName = resultSet.getString("name");
-            String resultDescription = resultSet.getString("description");
-            company = new Company(resultId, resultName, resultDescription);
+            if (resultSet.getMetaData() != null) {
+                while (resultSet.next()) {
+                    int compId = resultSet.getInt("id");
+                    String name = resultSet.getString("name");
+                    String description = resultSet.getString("description");
+                    company = new Company(compId, name, description);
+                }
+
+            }
             return company;
-        } catch (SQLException ex) {
+          } catch (SQLException | IOException ex) {
             System.out.println("Wrong column name or table");
             return null;
         }
+
     }
 
     @Override
     public void update(long id, Company company) {
         String sql = "UPDATE [company1] SET name= ?, description=? WHERE id=?";
         try {
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            PreparedStatement preparedStatement = Driver.getConnection().prepareStatement(sql);
             preparedStatement.setString(1, company.getCompanyName());
             preparedStatement.setString(2, company.getCompanyDescription());
             preparedStatement.setLong(3, company.getId());
             preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
+        } catch (SQLException | IOException ex) {
             System.out.println("Updating company is failed");
         }
     }
@@ -93,10 +100,10 @@ public class CompanyDalImpl implements CompanyDal {
     public void delete(long id) {
         String sql = "DELETE FROM [company1] WHERE id =  ?";
         try {
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            PreparedStatement preparedStatement = Driver.getConnection().prepareStatement(sql);
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
+        } catch (SQLException | IOException ex) {
             System.out.println("Deleting company is failed");
         }
     }
@@ -106,7 +113,7 @@ public class CompanyDalImpl implements CompanyDal {
         List<Company> company = new LinkedList<Company>();
         try {
             String sql = "SELECT id, name, description FROM [company1]";
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            PreparedStatement preparedStatement = Driver.getConnection().prepareStatement(sql);
             resultSet = preparedStatement.executeQuery();
             if (resultSet.getMetaData() != null) {
                 while (resultSet.next()) {
@@ -116,7 +123,7 @@ public class CompanyDalImpl implements CompanyDal {
                     company.add(new Company(id, name, description));
                 }
             }
-        } catch (SQLException ex) {
+        } catch (SQLException | IOException ex) {
             System.out.println("Wrong column name or table");
         }
         return company;

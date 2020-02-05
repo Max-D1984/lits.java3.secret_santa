fdab0f70a44cc48039b1fe6222068ed1b053902c
Max-D1984 <gjnnjl513319>
2020-02-05 23:20:14 +0200

    add web.xml spring-servlet.xml add MyCompanyController MyWishListController

---
+++ src/main/java/controller/MyCompanyListController.java
@@ -0,0 +1,31 @@
+package controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pojo.User;
+import service.CompanyService;
+import service.CompanyServiceImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/company")
+
+public class MyCompanyListController {
+    CompanyService comp = new CompanyServiceImpl();
+    @RequestMapping(
+            value = "/my-company",
+            method = RequestMethod.GET)
+    public ResponseEntity getMyCompany(
+            @RequestParam Integer id) {
+        return ResponseEntity.of(Optional.of(
+                comp.readCompany(id)));
+    }
+    @RequestMapping(
+            value = "/my-company/list",
+            method = RequestMethod.GET)
+    public ResponseEntity getMyCompanyList() {
+        return ResponseEntity.of(Optional.of(
+                comp.readCompanyList()));
+    }
+}
---
+++ src/main/java/controller/MyWishListController.java
@@ -0,0 +1,66 @@
+package controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pojo.User;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/wishlist")
+public class MyWishListController {
+    static {
+        System.out.println("here");
+    }
+    @RequestMapping(
+            value = "/my-wishlist",
+            method = RequestMethod.GET)
+    public ResponseEntity getMyWishList(
+            @RequestParam Integer id) {
+        return ResponseEntity.of(Optional.of(
+                        new User(id,
+                                "name",
+                                "Hello")));
+    }
+    @RequestMapping(
+            value = "/my-wishlist/list",
+            method = RequestMethod.GET)
+    public ResponseEntity getMyWishListList() {
+        return ResponseEntity.of(Optional.of(List.of(
+                new User(3,
+                        "name",
+                        "Hello"),
+                new User(3+3,
+                        "name3",
+                        "Hello3"))));
+    }
+    @RequestMapping(
+            value = "/my-wishlist",
+            method = RequestMethod.POST)
+    public ResponseEntity postMyWishList(
+            @RequestBody User user) {
+        return ResponseEntity.of(Optional.of(
+                new User(1,
+                        "name",
+                        "Hello")));
+    }
+    @RequestMapping(
+            value = "/my-wishlist",
+            method = RequestMethod.PUT)
+    public ResponseEntity putMyWishList(
+            @RequestBody User user) {
+        return ResponseEntity.of(Optional.of(
+                new User(1,
+                        "name",
+                        "Hello")));
+    }
+    @RequestMapping(
+            value = "/my-wishlist",
+            method = RequestMethod.DELETE)
+    public ResponseEntity deleteMyWishList(
+            @RequestParam Integer id) {
+        return ResponseEntity.of(Optional.of(
+                new User(1,
+                        "name",
+                        "Hello")));
+    }
+}

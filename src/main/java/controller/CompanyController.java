9cb0b9b5180215bf788684d1060c3ccf52514d18
Max-D1984 <is96071@gmail.com>
2020-02-11 14:54:08 +0200

    upd CompanyController CompanyDallImpl

--- src/main/java/controller/CompanyController.java
+++ src/main/java/controller/CompanyController.java
@@ -1,15 +1,13 @@
 package controller;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pojo.Company;
-import pojo.User;
 import service.CompanyService;
 import service.CompanyServiceImpl;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.List;
 import java.util.Optional;
+
 @EnableSwagger2
 @RestController
 @RequestMapping(value = "/company")
--- src/main/java/dal/CompanyDalImpl.java
+++ src/main/java/dal/CompanyDalImpl.java
@@ -13,14 +13,15 @@ public class CompanyDalImpl implements CompanyDal {
     public static final String TABLE_COMPANY_COLUMN_ID = "id";
     public static final String TABLE_COMPANY_COLUMN_NAME = "name";
     public static final String TABLE_COMPANY_COLUMN_DESCRIPTION = "description";
+    public static final String TABLE_NAME_COMPANY = "company";
     private ResultSet resultSet;
 
 
 
     @Override
     public void create(Company company) {
-        String sql = "INSERT INTO [company] (" + TABLE_COMPANY_COLUMN_NAME + ", " + TABLE_COMPANY_COLUMN_DESCRIPTION +
-                ") VALUES (?,?)";
+        String sql = "INSERT INTO [" + TABLE_NAME_COMPANY + "] (" + TABLE_COMPANY_COLUMN_NAME + ", " +
+                TABLE_COMPANY_COLUMN_DESCRIPTION + ") VALUES (?,?)";
         try {
             PreparedStatement preparedStatement = Driver.getConnection().prepareStatement(sql);
             preparedStatement.setString(1, company.getCompanyName());
@@ -36,7 +37,7 @@ public class CompanyDalImpl implements CompanyDal {
         Company company = null;
         try {
             String sql = "SELECT " + TABLE_COMPANY_COLUMN_ID + ", " + TABLE_COMPANY_COLUMN_NAME + ", " +
-                    TABLE_COMPANY_COLUMN_DESCRIPTION + " FROM [company] WHERE "+ TABLE_COMPANY_COLUMN_ID +"=?";
+                    TABLE_COMPANY_COLUMN_DESCRIPTION + " FROM [" + TABLE_NAME_COMPANY + "] WHERE " + TABLE_COMPANY_COLUMN_ID +"=?";
             PreparedStatement preparedStatement = Driver.getConnection().prepareStatement(sql);
             preparedStatement.setLong(1, id);
             resultSet = preparedStatement.executeQuery();
@@ -59,8 +60,8 @@ public class CompanyDalImpl implements CompanyDal {
 
     @Override
     public void update(Company oldCompany, Company newCompany) {
-        String sql = "UPDATE [company] SET " + TABLE_COMPANY_COLUMN_NAME + "= ?, " + TABLE_COMPANY_COLUMN_DESCRIPTION +
-                "=? WHERE " + TABLE_COMPANY_COLUMN_ID + "=?";
+        String sql = "UPDATE [" + TABLE_NAME_COMPANY + "] SET " + TABLE_COMPANY_COLUMN_NAME + "= ?, " +
+                TABLE_COMPANY_COLUMN_DESCRIPTION + "=? WHERE " + TABLE_COMPANY_COLUMN_ID + "=?";
         try {
             PreparedStatement preparedStatement = Driver.getConnection().prepareStatement(sql);
             preparedStatement.setString(1, newCompany.getCompanyName());
@@ -68,20 +69,20 @@ public class CompanyDalImpl implements CompanyDal {
             preparedStatement.setLong(3, oldCompany.getId());
             preparedStatement.executeUpdate();
         } catch (SQLException | IOException ex) {
-            System.out.println("Updating company is failed");
+            System.out.println("Updating " + TABLE_NAME_COMPANY + " is failed");
         }
     }
 
 
     @Override
     public void delete(Company company) {
-        String sql = "DELETE FROM [company] WHERE " + TABLE_COMPANY_COLUMN_ID + " =  ?";
+        String sql = "DELETE FROM [" + TABLE_NAME_COMPANY + "] WHERE " + TABLE_COMPANY_COLUMN_ID + " =  ?";
         try {
             PreparedStatement preparedStatement = Driver.getConnection().prepareStatement(sql);
             preparedStatement.setLong(1, company.getId());
             preparedStatement.executeUpdate();
         } catch (SQLException | IOException ex) {
-            System.out.println("Deleting company is failed");
+            System.out.println("Deleting " + TABLE_NAME_COMPANY + " is failed");
         }
     }
 
@@ -90,7 +91,7 @@ public class CompanyDalImpl implements CompanyDal {
         List<Company> company = new LinkedList<Company>();
         try {
             String sql = "SELECT " + TABLE_COMPANY_COLUMN_ID + ", " + TABLE_COMPANY_COLUMN_NAME + ", " +
-                    TABLE_COMPANY_COLUMN_DESCRIPTION + " FROM [company]";
+                    TABLE_COMPANY_COLUMN_DESCRIPTION + " FROM [" + TABLE_NAME_COMPANY + "]";
             PreparedStatement preparedStatement = Driver.getConnection().prepareStatement(sql);
             resultSet = preparedStatement.executeQuery();
             if (resultSet.getMetaData() != null) {

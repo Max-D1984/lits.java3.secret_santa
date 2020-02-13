6ae305fb12bf7caa137329a2706180a12c34f51c
Sedel Zoryana <zoryanasedel@gmail.com>
2020-02-13 20:16:57 +0200

    Add TargetWishList andpoint

--- src/main/java/controller/MyWishListController.java
+++ src/main/java/controller/MyWishListController.java
@@ -23,4 +23,14 @@ public class MyWishListController {
         List<MyWishListResponse> presentForUserId = userToPresentDal.readPresentListById(targetwishlist);
         return ResponseEntity.of(Optional.of(presentForUserId));
     }
+
+
+    @RequestMapping(
+            value = "/my-target-wishlist",
+            method = RequestMethod.GET)
+    public ResponseEntity getMyTargetWishListList(@RequestParam Integer targetwishlist) {
+        UserToPresentDal userToPresentDal = new UserToPresentDalImpl();
+        List<MyWishListResponse> presentForUserId = userToPresentDal.readPresentListById(targetwishlist);
+        return ResponseEntity.of(Optional.of(presentForUserId));
+    }
 }

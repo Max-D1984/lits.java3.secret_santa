de8a6e4cb84b18958f1dca6af8d4bf53e77498f0
Sedel Zoryana <zoryanasedel@gmail.com>
2020-02-13 21:17:21 +0200

    Add TargetWishList andpoint

--- src/main/java/controller/MyWishListController.java
+++ src/main/java/controller/MyWishListController.java
@@ -3,6 +3,8 @@ package controller;
 import dal.UserToPresentDal;
 import dal.UserToPresentDalImpl;
 import model.MyWishListResponse;
+import model.Present;
+import model.WishList;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.UserToPresentService;
@@ -11,16 +13,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @EnableSwagger2
 @RestController
-@RequestMapping(value = "/wishlist")
+@RequestMapping(value = "/wish-list")
 public class MyWishListController {
 
     private int loggedInUserId = 8;
 
     @RequestMapping(
-            value = "/my-wishlist/list",
+            value = "/my-wish-list/list",
             method = RequestMethod.GET)
     public ResponseEntity getMyWishListList() {
         UserToPresentService userToPresentDal = new UserToPresentServiceImpl();
@@ -30,11 +33,21 @@ public class MyWishListController {
 
 
     @RequestMapping(
-            value = "/my-target-wishlist",
+            value = "/my-target-wish-list",
             method = RequestMethod.GET)
     public ResponseEntity getMyTargetWishListList(@RequestParam Integer targetwishlist) {
-        UserToPresentDal userToPresentDal = new UserToPresentDalImpl();
-        List<MyWishListResponse> presentForUserId = userToPresentDal.readPresentListById(targetwishlist);
-        return ResponseEntity.of(Optional.of(presentForUserId));
+        UserToPresentService userToPresentService = new UserToPresentServiceImpl();
+        List<MyWishListResponse> presentForUserId = userToPresentService.readPresentListById(targetwishlist);
+
+        List<Present> presentList = presentForUserId.stream()
+                .map(presentFromDal -> new Present(presentFromDal.getName(), presentFromDal.getUrl()))
+                .collect(Collectors.toList());
+
+
+        WishList wishList = new WishList();
+        wishList.setHobbyList(null);
+        wishList.setPresentList(presentList);
+
+        return ResponseEntity.of(Optional.of(wishList));
     }
 }

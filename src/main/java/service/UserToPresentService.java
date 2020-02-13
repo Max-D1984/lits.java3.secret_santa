0ff99c581add2ad50520a429750bc2322d0d9996
olehkhalus <oleh0704@gmail.com>
2020-02-13 18:19:49 +0200

    Create UserToPResentService and UserToPresentServiceImpl

---
+++ src/main/java/service/UserToPresentService.java
@@ -0,0 +1,16 @@
+package service;
+
+import model.MyWishListResponse;
+import pojo.UserToPresent;
+
+import java.util.List;
+
+public interface UserToPresentService {
+    void create (UserToPresent userToPresent);
+    UserToPresent read (int id);
+    List<UserToPresent> readByUser(int id);
+    void update (int id, UserToPresent userToPresent);
+    void delete (int id);
+    List<UserToPresent> readList ();
+    List<MyWishListResponse> readPresentListById(int id);
+}
---
+++ src/main/java/service/UserToPresentServiceImpl.java
@@ -0,0 +1,46 @@
+package service;
+
+import dal.UserToPresentDal;
+import dal.UserToPresentDalImpl;
+import model.MyWishListResponse;
+import pojo.UserToPresent;
+
+import java.util.List;
+
+public class UserToPresentServiceImpl implements UserToPresentService {
+    private UserToPresentDal userToPresentDal = new UserToPresentDalImpl();
+    @Override
+    public void create(UserToPresent userToPresent) {
+        userToPresentDal.create(userToPresent);
+    }
+
+    @Override
+    public UserToPresent read(int id) {
+        return userToPresentDal.read(id);
+    }
+
+    @Override
+    public List<UserToPresent> readByUser(int id) {
+        return userToPresentDal.readByUser(id);
+    }
+
+    @Override
+    public void update(int id, UserToPresent userToPresent) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<UserToPresent> readList() {
+        return userToPresentDal.readList();
+    }
+
+    @Override
+    public List<MyWishListResponse> readPresentListById(int id) {
+        return userToPresentDal.readPresentListById(id);
+    }
+}

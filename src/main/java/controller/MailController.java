7f9b6d1736a3e4b2fe088a549e2806e3aa85ca96
olehkhalus <oleh0704@gmail.com>
2020-02-19 21:03:01 +0200

    Update MailController

--- src/main/java/controller/MailController.java
+++ src/main/java/controller/MailController.java
@@ -1,10 +1,15 @@
 package controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import pojo.Company;
 import pojo.UserToCompany;
 import service.CompanyService;
@@ -15,6 +20,8 @@ import service.UserToCompanyServiceImpl;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
@@ -72,12 +79,24 @@ public class MailController {
             @RequestParam String mailTo,
             @RequestParam String username,
             @RequestParam String password,
-            @RequestParam Integer companyId) {
+            @RequestParam Integer companyId,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+
         List<UserToCompany> currentUserList = userToCompanyService.readListByCompanyId(companyId);
-        String url = "https://lits-java3-secret-santa.herokuapp.com/user/userlist?id=" + companyId;
-        String url2 = "http://localhost:8080/secret-santa/user/userlist?id=" + companyId;
-        for (UserToCompany users : currentUserList) {
-            if (users.getId() == loggedInUserId && users.getRole().equals("admin")) {
+        StringBuilder urlToCompany = new StringBuilder();
+
+        HttpRequest httpRequest = new ServletServerHttpRequest(request);
+        UriComponents uriComponents = UriComponentsBuilder.fromHttpRequest(httpRequest).build();
+        String scheme = uriComponents.getScheme();
+        String host = uriComponents.getHost();
+        if(host.equals("localhost")){
+            System.out.println("it is localhost");
+            urlToCompany.append("http://localhost:8080/secret-santa/user/userlist?id=" + companyId);
+        }else{
+            urlToCompany.append("https://lits-java3-secret-santa.herokuapp.com/user/userlist?id=" + companyId);
+        }
+            if (currentUserList.get(loggedInUserId).getRole().equals("admin")) {
                 Properties props = new Properties();
                 CompanyService companyService = new CompanyServiceImpl();
                 props.put("mail.smtp.auth", true);
@@ -104,7 +123,7 @@ public class MailController {
                             + "\n"
                             + "I want to invite you to participate in a Secret Santa game to my company " + company.getCompanyName()
                             + "\n"
-                            + "If you want to confirm, please, follow this link " + url2);
+                            + "If you want to confirm, please, follow this link " + urlToCompany);
                     Transport.send(message);
                     System.out.println("Mail Sent Successfully");
                 } catch (AuthenticationFailedException ex) {
@@ -115,7 +134,6 @@ public class MailController {
                 return ResponseEntity.of(Optional.of(
                         "Mail was send to " + mailTo));
             }
-        }
         return ResponseEntity.of(Optional.of(
                 "Mail sending was failed"
         ));

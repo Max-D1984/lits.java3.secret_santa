720693c4170e485771e327b83f5980fb8b886f21
Max-D1984 <is96071@gmail.com>
2020-02-21 09:08:11 +0200

    upd web.xml for webUI

--- src/main/java/controller/CompanyController.java
+++ src/main/java/controller/CompanyController.java
@@ -1,11 +1,17 @@
 package controller;
 
 import application.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pojo.Company;
 import pojo.UserToCompany;
-import service.*;
+
+import service.CompanyService;
+import service.UserToCompanyService;
+import service.UserService;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
@@ -18,17 +24,35 @@ import java.util.Optional;
 public class CompanyController {
 
 
-    CompanyService comp = new CompanyServiceImpl();
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private CompanyService companyService;
+    @Autowired
+    private UserToCompanyService userToCompanyService;
+
+    public UserToCompanyService getUserToCompanyService() {
+        return userToCompanyService;
+    }
+
+    public CompanyService getCompanyService() {
+        return companyService;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
     @RequestMapping(
             value = "/users-company",
             method = RequestMethod.GET)
     public ResponseEntity getMyCompanys(
             @RequestParam Integer user_id) {
-        UserToCompanyService userToCompanyService = new UserToCompanyServiceImpl();
+
         List<Integer> ids = userToCompanyService.getCompanysByUserId(Constants.LOGGEDUSER);
-               comp.getUsersCompany(ids);
+        companyService.getUsersCompany(ids);
         return ResponseEntity.of(Optional.of(
-                comp.getUsersCompany(ids)));
+                companyService.getUsersCompany(ids)));
     }
 
     @RequestMapping(
@@ -36,32 +60,32 @@ public class CompanyController {
             method = RequestMethod.GET)
     public ResponseEntity getExactCompany(
             @RequestParam Integer company_id) {
-               return ResponseEntity.of(Optional.of(
-                       comp.readCompany(company_id)));
+        return ResponseEntity.of(Optional.of(
+                companyService.readCompany(company_id)));
     }
 
     @RequestMapping(
             value = "/my-company/list",
             method = RequestMethod.GET)
     public ResponseEntity getMyCompanyList() {
-
         return ResponseEntity.of(Optional.of(
-                comp.readCompanyList()));
+                companyService.readCompanyList()));
     }
+
     @RequestMapping(
             value = "/company",
             method = RequestMethod.POST)
-    public ResponseEntity postCompany( @RequestParam String name, String description, int loggedUserId) {
-        CompanyService companyService = new CompanyServiceImpl();
-        Company newCompany = new Company(0,"SomeName", "SomeDescription");
+    public ResponseEntity postCompany(@RequestParam String name, @RequestParam String description, @RequestParam Integer loggedUserId) {
+
+        Company newCompany = new Company(0, "SomeName", "SomeDescription");
         companyService.createCompany(newCompany);
         List<Company> allCompany = companyService.readCompanyList();
-        Company lastCompany = allCompany.get(allCompany.size()-1);
-        UserToCompanyService userToCompanyService = new UserToCompanyServiceImpl();
-        userToCompanyService.createUserToCompany(new UserToCompany(0, Constants.LOGGEDUSER,lastCompany.getId(),"admin"));
-        UserService userService = new UserServiceImpl();
+        Company lastCompany = allCompany.get(allCompany.size() - 1);
+
+        userToCompanyService.createUserToCompany(new UserToCompany(0, Constants.LOGGEDUSER, lastCompany.getId(), "admin"));
+
         return ResponseEntity.of(Optional.of(
-               "Created company " + lastCompany.getCompanyName() + " admin: " +  userService.readUser( Constants.LOGGEDUSER)));
+                "Created company " + lastCompany.getCompanyName() + " admin: " + userService.readUser(Constants.LOGGEDUSER)));
     }
 
     @RequestMapping(
@@ -69,9 +93,9 @@ public class CompanyController {
             method = RequestMethod.PUT)
     public ResponseEntity putCompany(@RequestParam int id, String newName, String newDescription) {
         Company newCompany = new Company(id, newName, newDescription);
-        Company oldCompany = comp.readCompany(id);
-       comp.updateCompany(oldCompany,newCompany);
-        return ResponseEntity.of(Optional.of( "Update company" + oldCompany + " to " + newCompany));
+        Company oldCompany = companyService.readCompany(id);
+        companyService.updateCompany(oldCompany, newCompany);
+        return ResponseEntity.of(Optional.of("Update company" + oldCompany + " to " + newCompany));
     }
 
     @RequestMapping(
@@ -79,10 +103,10 @@ public class CompanyController {
             method = RequestMethod.DELETE)
     public ResponseEntity deleteCompany(
             @RequestParam Integer id) {
-        Company deletedCOmpany = comp.readCompany(11);
-        comp.deleteCompany(deletedCOmpany);
+        Company deletedCOmpany = companyService.readCompany(11);
+        companyService.deleteCompany(deletedCOmpany);
         return ResponseEntity.of(Optional.of(
-                "Deleted company "+ deletedCOmpany
+                "Deleted company " + deletedCOmpany
         ));
     }
 

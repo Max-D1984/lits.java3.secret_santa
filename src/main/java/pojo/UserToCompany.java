961402aa2740f975bbfb71dc5953ac4f612072ad
olehkhalus <oleh0704@gmail.com>
2020-02-27 20:28:27 +0200

    Add JPA annotations

--- src/main/java/pojo/UserToCompany.java
+++ src/main/java/pojo/UserToCompany.java
@@ -1,12 +1,51 @@
 package pojo;
 
+import javax.persistence.*;
 import java.util.Objects;
 
+/*
+Перелік JPA-анотацій (Java Persistence API) для реалізації ORM(Object-Relational Mapping) технології:
+
+@Entity - вказує, що наш клас є певною сутністю
+
+@Table - вказує на конкретну таблицю з БД для відображення відповідної сутності(в параметрі для поля "name"
+вказуєм ту назву таблиці, яка вказана в БД
+
+@Id - вказує, що це поле є первинним ключем (primary key), тобто це поле буде використовуватись
+для ідентифікації кожного унікального запису в БД
+
+@Column - вказує на звязок між полем нашого pojo-класу і відповідним стовбцем таблиц в БД. Якщо назва поля і
+стовбця співпадають - анотацію можна не вказувати (рекомендую всерівно вказувати, щоб не було конфліктів).
+В параметрі для поля "name" вказуєм ту назву, яка вказана для відповідного стовбця в БД.
+
+@GeneratedValue - відповідає за автоінкрементацію (конкретно в нашому випадку:
+                  збільшення значення поля id на 1 із кожним новим записом в БД)
+
+* Потрібно створити НЕприватний конструктор без параметрів
+
+ */
+
+@Entity
+@Table(name = "user_to_company")
 public class UserToCompany {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "user_id")
     private int user_id;
+
+    @Column(name = "company_id")
     private int company_id;
+
+    @Column(name = "role")
     private String role;
+
+    //НЕприватний конструктор без параметрів
+    public UserToCompany() {
+    }
 
     public UserToCompany(int id, int user_id, int company_id, String role) {
         this.id = id;

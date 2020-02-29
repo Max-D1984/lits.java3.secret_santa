9adbfaf999f9e9a375affafe5e8870a8ec0f4924
olehkhalus <oleh0704@gmail.com>
2020-02-29 14:13:12 +0200

    Add fields email and password

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -1,14 +1,35 @@
 package pojo;
 
+
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "name")
     private String userName;
 
-    public User(int id, String userName) {
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String passWord;
+
+    public User() {
+    }
+
+    public User(int id, String userName, String email, String passWord) {
         this.id = id;
         this.userName = userName;
+        this.email = email;
+        this.passWord = passWord;
     }
 
     public int getId() {
@@ -27,18 +48,36 @@ public class User {
         this.userName = userName;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
         return id == user.id &&
-                Objects.equals(userName, user.userName);
+                Objects.equals(userName, user.userName) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(passWord, user.passWord);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, userName);
+        return Objects.hash(id, userName, email, passWord);
     }
 
     @Override
@@ -46,8 +85,11 @@ public class User {
         return "User{" +
                 "id=" + id +
                 ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", passWord='" + passWord + '\'' +
                 '}';
     }
+
 
 }
 

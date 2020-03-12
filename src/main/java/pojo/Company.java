9944a88da62a89914411fa9ea0c1540341883f8d
Oleh Khalus <oleh0704@gmail.com>
2020-03-12 19:32:30 +0200

    Add new field image for Company

--- src/main/java/pojo/Company.java
+++ src/main/java/pojo/Company.java
@@ -1,5 +1,6 @@
 package pojo;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Company {
@@ -7,59 +8,75 @@ public class Company {
     private int id;
     private String companyName;
     private String companyDescription;
+    private byte[] image;
 
-
-    public Company(int id, String companyName, String companyDescription) {
+    public Company(int id, String companyName, String companyDescription, byte[] image) {
         this.id = id;
         this.companyName = companyName;
         this.companyDescription = companyDescription;
+        this.image = image;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public String getCompanyDescription() {
-        return companyDescription;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public void setCompanyDescription(String companyDescription) {
-        this.companyDescription = companyDescription;
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", companyName='" + companyName + '\'' +
+                ", companyDescription='" + companyDescription + '\'' +
+                ", image=" + Arrays.toString(image) +
+                '}';
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Company that = (Company) o;
-        return id == that.id &&
-                Objects.equals(companyName, that.companyName) &&
-                Objects.equals(companyDescription, that.companyDescription);
+        Company company = (Company) o;
+        return id == company.id &&
+                Objects.equals(companyName, company.companyName) &&
+                Objects.equals(companyDescription, company.companyDescription) &&
+                Arrays.equals(image, company.image);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, companyName, companyDescription);
+        int result = Objects.hash(id, companyName, companyDescription);
+        result = 31 * result + Arrays.hashCode(image);
+        return result;
     }
 
-    @Override
-    public String toString() {
-        return "Company_records{" +
-                "id=" + id +
-                ", companyName='" + companyName + '\'' +
-                ", companyDescription='" + companyDescription + '\'' +
-                '}';
+    public int getId() {
+        return id;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyDescription() {
+        return companyDescription;
+    }
+
+    public void setCompanyDescription(String companyDescription) {
+        this.companyDescription = companyDescription;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+
+
 }

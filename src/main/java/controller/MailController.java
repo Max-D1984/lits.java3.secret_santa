3358eb34c0966f2d153955d6f657ce85e665fa58
Panda1958 <andy.panda1958@gmail.com>
2020-03-18 10:39:03 +0300

    udd MailController

--- src/main/java/controller/MailController.java
+++ src/main/java/controller/MailController.java
@@ -233,4 +233,35 @@ public class MailController {
                 "Mail was send to " + mailTo));
     }
 
+
+    public boolean sendEmail (String mailTo, String username, String password, String mailText) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.ssl.enable", true);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getInstance(props,
+
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("edulitsjava@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(mailTo));
+
+            message.setText(mailText);
+            Transport.send(message);
+            System.out.println("Mail Sent Successfully");
+        } catch (AuthenticationFailedException ex) {
+            throw new RuntimeException(ex);
+        } catch (MessagingException ex) {
+            throw new RuntimeException(ex);
+        }
+        return true;
+    }
 }
